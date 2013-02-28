86d9b0d9387ab75f69e3a8bfb19ccb93d9ef2167
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-02-28 05:36:07 +0400

    embed heroku settings

--- CourseRu/settings.py
+++ CourseRu/settings.py
@@ -13,11 +13,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'djdb',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'd40lee7u23qefp',                      # Or path to database file if using sqlite3.
+        'USER': 'qbwvskbxqdcjhp',                      # Not used with sqlite3.
+        'PASSWORD': 'mDb_oNrSnupLP6jp6y2i-LrcmS',                  # Not used with sqlite3.
+        'HOST': 'ec2-54-243-184-143.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5542',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -167,12 +167,6 @@ EMAIL_USE_TLS = True
 FIXTURE_DIRS = (
     './Fixtures',
 )
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-import os
-print os.getenv("$DATABASE_URL")
-DATABASES['default'] = dj_database_url.config(default='sqlite:////djdb.sqlite')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
