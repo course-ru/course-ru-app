81f3dca0f5025e242b28c77612a63e881165cf1b
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-03-01 02:20:44 +0400

    oh hell i am tired

--- CourseRu/settings.py
+++ CourseRu/settings.py
@@ -21,16 +21,13 @@ MANAGERS = ADMINS
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djdb',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+
+import dj_database_url
+import os
+a = os.getenv("DATABASE_URL")
+print a
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='sqlite:////djdb.sqlite')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -178,9 +175,6 @@ EMAIL_USE_TLS = True
 FIXTURE_DIRS = (
     './Fixtures',
 )
-
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
