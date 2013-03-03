1116ad25aa443fad16d20da09bdee9252aed02c7
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-03-03 19:52:02 +0400

    adding more test data with images

---
+++ student/urls.py
@@ -0,0 +1,14 @@
+﻿from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('main.views',
+                       url(r'^index/$', 'index'),
+                       url(r'^about/$', 'about', name='about'),
+                       url(r'^courses/(?P<course_id>\d+)/$', 'course', name='course'),
+                       url(r'^courses/(?P<course_id>\d+)/apply/$', 'apply_for_course', name='apply_for_course'),
+)
+
+urlpatterns += patterns('', url(r'^admin/', include(admin.site.urls)))
