9e9bc0be07567b47323606c4fb37fbd4c23ecb8a
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-03-03 17:24:38 +0400

    moving media folder to root; add some tricky hack to project urls.py

--- CourseRu/settings.py
+++ CourseRu/settings.py
@@ -47,7 +47,7 @@ MEDIA_ROOT = './media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'localhost:8000/media/'
+MEDIA_URL = '/media/'
 
 ADMIN_MEDIA_PREFIX = '/media/'
 
--- CourseRu/urls.py
+++ CourseRu/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -9,3 +10,9 @@ urlpatterns = patterns('',
                        url(r'^accounts/', include('accounts.urls')),
                        url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+                            (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                                'document_root': settings.MEDIA_ROOT}))
